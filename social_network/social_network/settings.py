3fce234df584710b753bf5fa90788558c6fa3482
dmytrorevak <dmytro.revak@gmail.com>
2017-10-05 13:46:47 +0300

    register models in admin and add few recirds

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_network_db',
+        'USER': 'social_network_user',
+        'PASSWORD': '123456Qwerty!',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- social_network/social_network_app/models.py
+++ social_network/social_network_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='user')
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserProfile,
+                                on_delete=models.CASCADE,
+                                related_name='sender')
+
+    receiver = models.ForeignKey(UserProfile,
+                                on_delete=models.CASCADE,
+                                related_name='receiver')
+
+    message_body = models.TextField(blank=False)
+    date = models.DateField()
+
+
+class FriendsRecord(models.Model):
+    first_friend = models.ForeignKey(UserProfile,
+                                    on_delete=models.CASCADE,
+                                    related_name='first_friend')
+
+    second_friend = models.ForeignKey(UserProfile,
+                                    on_delete=models.CASCADE,
+                                    related_name='second_friend')
