4dc4f3e793aa9dc6b1f0bf17f8556d4c406f78c3
dmytrorevak <dmytro.revak@gmail.com>
2017-10-09 17:27:47 +0300

    add registration view

--- social_network/social_network_app/forms.py
+++ social_network/social_network_app/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import *
 
 
-class UserForm(UserCreationForm):
+class UserForm(forms.ModelForm):
     class Meta:
-        model = UserProfile
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('phone',)
