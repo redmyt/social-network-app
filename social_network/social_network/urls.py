d17619803a469a4d5013592fdd960877c9a6907d
dmytrorevak <dmytro.revak@gmail.com>
2017-10-10 21:49:28 +0300

    add message view

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.start_page_view, name='start_page'),
     url(r'^user/$', views.user_page_view, name='user_page'),
+    url(r'^login/$', views.login_view, name='login'),
     url(r'^messages/$', views.messages_view, name='messages'),
     url(r'^friends/$', views.friends_view, name='friends'),
 ]
